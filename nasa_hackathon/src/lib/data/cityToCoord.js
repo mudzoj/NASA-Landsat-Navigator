b981f61c950107e0623bc88937d6e72b348d6d5f
sammi <sshih5678@gmail.com>
2024-10-06 11:21:24 -0700

    add city coords

---
+++ nasa_hackathon/src/lib/data/cityToCoord.js
@@ -0,0 +1,7 @@
+export const cityToCoord = {
+  vancouver: { path: 47, row: 26 },
+  newYork: { path: 13, row: 32 },
+  losAngeles: { path: 41, row: 36 },
+  toronto: { path: 18, row: 30 },
+  chicago: { path: 23, row: 31 },
+};
